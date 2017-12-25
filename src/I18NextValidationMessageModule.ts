b3670727fab1f527fb203fc7e375ff99917718ce
Romanchuk <rezety@gmail.com>
2017-12-25 22:59:46 +0700

    initial commit

---
+++ src/I18NextValidationMessageModule.ts
@@ -0,0 +1,31 @@
+
+import { NgModule, ModuleWithProviders, Type } from '@angular/core';
+import { I18NextModule } from 'angular-i18next';
+
+import { ValidationMessageModule, ValidationMessageComponent } from 'angular-validation-message';
+
+import { I18NextValidationMessageComponent } from './components/I18NextValidationMessageComponent';
+
+
+export const declarations = [
+  I18NextValidationMessageComponent,
+];
+
+export const providers = [
+  {
+    provide: ValidationMessageComponent,
+    useValue: I18NextValidationMessageComponent
+  }
+];
+
+@NgModule({
+  declarations: declarations,
+  exports: declarations,
+  entryComponents: declarations,
+  imports: [
+    I18NextModule,
+    ValidationMessageModule
+  ],
+  providers: providers
+})
+export class I18NextValidationMessageModule {}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from './components/I18NextValidationMessageComponent';
+export * from './I18NextValidationMessageModule';
